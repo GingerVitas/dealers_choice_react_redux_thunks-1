f9ac66503730bb5f4f581494ab2b1dfdbb452d1c
Stefan <sm9dw@virginia.edu>
2022-03-17 05:45:39 -0400

    store set up, need to connect everything

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
-import AddInventory from '../react-redux';
+import store from '../store';
+import {connect, Provider} from 'react-redux'
+import { addItems } from "../store";
+const LIST_ITEMS = 'LIST_ITEMS'
 
 
-class Temp extends Component{
+const mapDispatchtoProps = (dispatch) =>{
+    return {
+        loadItems: () => {
+            dispatch(loadItems())
+        }
+    }
+}
+
+class _App extends Component{
+    constructor(props) {
+        super(props);
+        this.state = store.getState();
+    }
+    async componentDidMount(){
+        const addItems = this.props
+        addItems();
+    }
     render(){
         return (
-            <div>
-                <h1>Website Coming Soon</h1>
+            <div className="Inventory">
+                <h1>Acme Inventory Management</h1>
+                <input type='text' value={this.state.input}>
+                </input>
+                <button className="item">Add to Tracker</button>
             </div>
         )
     }
 }
 
+const App = connect(null, mapDispatchtoProps)(_App);
 
-render(<AddInventory />, document.getElementById('root'));
+
+render(<Provider store = {store}><App /></Provider>, document.getElementById('root'));
--- store.js
+++ store.js
@@ -1,11 +1,40 @@
-import {createStore } from 'redux';
+import axios from 'axios';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger'
+import {createStore, applyMiddleware, combineReducers } from 'redux';
 
+const ADD_INVENTORY = 'ADD_INVENTORY'
 
-const reducer = (state, action)=> {
+const _addItems = (items) => {
+    return {
+        type: ADD_INVENTORY,
+        items
+    }
+}
+
+const addItems = () => {
+    return async (dispatch) => {
+        const items = (await axios.get('/api/products')).data;
+        dispatch(_addItems(items))
+    }
+}
+
+const inventoryReducer = (state = [], action)=> {
+    if(action.type === ADD_INVENTORY) {
+        return action.items;
+    }
     return state;
 };
 
 
-const store = createStore(reducer);
+const reducer = combineReducers({
+    items: inventoryReducer
+})
+
+const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 export default store;
+
+export{
+    addItems
+}
